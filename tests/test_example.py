ecf906529a1558bcc464c6a9ab4c60badda55238
Saddem Yousfi <saddemyoussfi@gmail.com>
2026-01-07 11:28:26 +0100

    Fixed error

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,4 +5,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from app import add
 
 def test_basic_math():
-    assert 1 + 1 == 3   # ❌ deliberate failure
+    assert 1 + 1 == 2  # correct
